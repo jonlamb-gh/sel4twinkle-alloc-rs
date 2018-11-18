52f292e34673fc665a4ae0e3611ec1ea41391237
Jon Lamb <lamb.jon.io@gmail.com>
2018-11-18 05:35:37 -0800

    Add some simple dma page utils

--- src/lib.rs
+++ src/lib.rs
@@ -24,7 +24,7 @@ pub mod vka_object;
 mod vspace;
 
 pub use init_cap::{InitCap, NUM_INITIAL_CAPS};
-pub use pmem::PMem;
+pub use pmem::{DMACacheOp, PMem};
 
 pub const MIN_UNTYPED_SIZE: usize = 4;
 pub const MAX_UNTYPED_SIZE: usize = 32;
--- src/pmem.rs
+++ src/pmem.rs
@@ -2,13 +2,23 @@
 // will just consume entire untyped with paddr to beginning until then
 
 use super::{Allocator, Error, PAGE_BITS_4K, PAGE_SIZE_4K};
+use object_type::ObjectType;
 use sel4_sys::*;
+use vka_object::VkaObject;
 
 /// Physical memory
 #[derive(Debug, Copy, Clone)]
 pub struct PMem {
     pub vaddr: seL4_Word,
     pub paddr: seL4_Word,
+    // TODO - pd_cap and/or frame cap?
+}
+
+#[derive(Debug, Copy, Clone)]
+pub enum DMACacheOp {
+    Clean,
+    Invalidate,
+    CleanInvalidate,
 }
 
 impl Allocator {
@@ -29,11 +39,11 @@ impl Allocator {
             vaddr,
             // rights,
             unsafe { seL4_CapRights_new(1, 1, 1) },
-            // not cacheable
-            0,
+            // TODO - not cacheable
+            // 0,
+            seL4_ARM_VMAttributes_seL4_ARM_Default_VMAttributes,
         )?;
 
-        // provide cap to the first frame
         if let Some(cap) = cap {
             *cap = obj.cptr;
         }
@@ -42,5 +52,101 @@ impl Allocator {
             vaddr,
             paddr: result.paddr,
         })
+    }
+
+    pub fn pmem_new_dma_page(&mut self, cap: Option<&mut seL4_CPtr>) -> Result<PMem, Error> {
+        let size_bits = PAGE_BITS_4K;
+
+        let ut: VkaObject = self.vka_alloc_untyped(size_bits as _)?;
+
+        let result: seL4_ARM_Page_GetAddress_t = unsafe { seL4_ARM_Page_GetAddress(ut.cptr) };
+        if result.error != 0 {
+            return Err(Error::Other);
+        }
+
+        let frame_cap = self.vka_cspace_alloc()?;
+        let mut path = self.vka_cspace_make_path(frame_cap);
+
+        let err = unsafe {
+            seL4_Untyped_Retype(
+                ut.cptr,
+                ObjectType::ARM_SmallPageObject.into(),
+                size_bits as _,
+                path.root,
+                path.dest,
+                path.dest_depth,
+                path.offset,
+                1,
+            )
+        };
+        if err != 0 {
+            return Err(Error::ResourceExhausted);
+        }
+
+        let frame_vaddr = self.vspace_new_pages(
+            1,
+            PAGE_BITS_4K as _,
+            unsafe { seL4_CapRights_new(1, 1, 1) },
+            seL4_ARM_VMAttributes_seL4_ARM_Default_VMAttributes,
+            Some(&mut path.cap_ptr),
+        )?;
+
+        /* TODO
+        let dest_paddr = base_paddr + (i as seL4_Word * PAGE_SIZE_4K);
+        let _frame_vaddr = self.vspace_new_pages_at(
+            Some(dest_paddr),
+            1,
+            PAGE_BITS_4K as _,
+            unsafe { seL4_CapRights_new(1, 1, 1) },
+            seL4_ARM_VMAttributes_seL4_ARM_Default_VMAttributes,
+            false,
+            Some(&mut path.cap_ptr),
+        )?;
+        */
+
+        if let Some(cap) = cap {
+            *cap = path.cap_ptr;
+        }
+
+        Ok(PMem {
+            vaddr: frame_vaddr,
+            paddr: result.paddr,
+        })
+    }
+
+    pub fn dma_cache_op(&self, vaddr: seL4_Word, size: usize, op: DMACacheOp) {
+        let root = self.vspace_get_root();
+        let end = vaddr + size as seL4_Word;
+        let mut cur = vaddr;
+
+        while cur < end {
+            let mut top = round_up(cur as usize + 1, PAGE_SIZE_4K as _) as seL4_Word;
+            if top > end {
+                top = end;
+            }
+
+            let err = match op {
+                DMACacheOp::Clean => unsafe {
+                    seL4_ARM_PageGlobalDirectory_Clean_Data(root, cur, top)
+                },
+                DMACacheOp::Invalidate => unsafe {
+                    seL4_ARM_PageGlobalDirectory_Invalidate_Data(root, cur, top)
+                },
+                DMACacheOp::CleanInvalidate => unsafe {
+                    seL4_ARM_PageGlobalDirectory_CleanInvalidate_Data(root, cur, top)
+                },
+            };
+            assert!(err == 0, "DMA ops failed");
+
+            cur = top;
+        }
+    }
+}
+
+fn round_up(val: usize, base: usize) -> usize {
+    val + if val % base == 0 {
+        0
+    } else {
+        base - (val % base)
     }
 }
